332818ca233b4982b1caed7dba116bda413649ab
eomsteve <mingyov0807@gmail.com>
2021-12-14 21:10:44 +0900

    선언과 할당

---
+++ varJs/Dod.js
@@ -0,0 +1,13 @@
+let value; // value라는 변수를 선언한다.
+
+value= 10; //선언한 변수 value에 10의 값을 할당한다.
+
+console.log(`할당된 변수 vaule 의 값은 : ${value}`);
+value = 20; //value변수에 다른 값을 재 할당한다.
+
+console.log(`재 할당된 변수 vaule 의 값은 : ${value}`);
+
+const pi = 3.14; //pi라는 변수를 const 로 선언한다.
+//할당이 동시에 이루어지지 않으면 오류가 나타난다.
+
+console.log(`할당된 변수 pi 의 값은 : ${pi}`);
