553c98b93e796f5e0f581227dcf1df167fc1404b
eomsteve <mingyov0807@gmail.com>
2021-12-22 04:41:09 +0900

    string

---
+++ string/dod.js
@@ -0,0 +1,13 @@
+let string = 'hello world';
+const N = 4;
+
+console.log('hello world'.charAt(N));
+console.log('hello world'[N]);
+
+let first = 'sh';
+let last = 'eom';
+
+//문자열 연결
+console.log('My name is ' + first + ' ' + last + '.'); // My name is sh eom.
+//표현식 삽입
+console.log(`My name is ${first} ${last}.`); // My name is sh eom.
